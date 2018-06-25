5d72be5f2e28b3a35211e789323531428769d361
Yuriy Yakymenko <7fold@ukr.net>
2018-06-24 22:21:06 -0400

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,108 @@ $('.menu-btn').on('click', function(e) {
   $('.menu-nav').toggleClass('menu-nav_active');
 });
 
+function start (lat, lng) {
+  $.ajax({
+    // url: "raw.json"
+    // url: "api/hello"
+    url: "https://yuriy.ca/recharge/?api_key=qweqwe123&lat="+ lat +"&lng="+ lng
+  }).done(function(resp) {
+    var latLng = {lat, lng};
+    //var data = $.parseJSON(resp)
+    // console.log(resp);
+
+  var locations = [];
+  // for(var i=0; i < data.length; i++){
+  //   var smth = [data[i].LocName, data[i].Lat, data[i].Long, data[i].LocID,
+  //               data[i].PortsUnknown, data[i].PortsAvail, data[i].PortsInUse ,data[i].PortsWorking,
+  //               data[i].PortsOffline, data[i].Level3Ports, data[i].Level2Ports ,data[i].Level1Ports,
+  //               data[i].Network, data[i].AccessCode, data[i].LocationTag];
+  //     locations.push(smth);
+  // };
+    for(var i=0; i < resp.length; i++){
+      var smth = [resp[i].id, resp[i].title, resp[i].address, resp[i].town, resp[i].country, resp[i].lat, resp[i].lng, 
+                  resp[i].postalcode, resp[i].phone, resp[i].numberofpoints, resp[i].comment, resp[i].powerkw];
+      locations.push(smth);
+  };
+  console.log(locations);
+  function initGoogleMap(){
+      var bounds  = new window.google.maps.LatLngBounds();
+      var image = {url: 'charger.png'};
+      var infowindow = new window.google.maps.InfoWindow();
+      var map = new window.google.maps.Map(document.getElementById('map'), {
+        zoom: 8,
+        center: new window.google.maps.LatLng(latLng),
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: window.google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: window.google.maps.ControlPosition.TOP_CENTER
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: window.google.maps.ControlPosition.LEFT_CENTER
+        },
+        scaleControl: true,
+        streetViewControl: true,
+        streetViewControlOptions: {
+            position: window.google.maps.ControlPosition.LEFT_TOP
+        },
+        fullscreenControl: true,
+        fullscreenControlOptions: {
+          position: window.google.maps.ControlPosition.BOTTOM_CENTER
+        }
+            });
+    function placeMarker(loc) {
+      var latLng = new window.google.maps.LatLng( loc[5], loc[6]);
+      var marker = new window.google.maps.Marker({
+        position : latLng,
+        map      : map,
+        icon     : image,
+        animation:  window.google.maps.Animation.DROP
+      });
+      var locat = new window.google.maps.LatLng(marker.position.lat(), marker.position.lng());
+      bounds.extend(locat);
+      map.fitBounds(bounds);      
+      map.panToBounds(bounds); 
+      window.google.maps.event.addListener(marker, 'click', function(){
+          map.setZoom(14);
+          map.setCenter(this.getPosition());
+          infowindow.close(); // Close previously opened infowindow
+          infowindow.setContent( "<div id='infowindow'>"+ loc[1] +"</div>");
+          
+          var info =
+          "<h3>" + loc[1].toUpperCase() + "</h3>" +
+          "<div class='_info'>" + 
+            "<div class='_row'>" +
+              "<div class='_col'>" +
+                "<p><b>Address:</b> " + loc[2] + "</p>" +
+                "<p><b>City:</b>  " + loc[3] + "</p>" +
+                "<p><b>Postal Code:</b>  " + loc[7] + "</p>" +
+              "</div>" +
+              "<div class='_col'>" +
+                "<p><b>Phone:</b>  " + loc[8] + "</p>" +
+                "<p><b>Comments:</b>  " + loc[10] + "</p>" +
+                "<p><b>Ports available:</b>  " + loc[9] + "</p>" +
+              "</div>" +
+              
+            "</div>" +
+          "</div>";
+         
+          
+          
+          $('#information').html(info);
+          infowindow.open(map, marker);
+      });
+    }
+    for(var i=0; i<locations.length; i++) {
+      placeMarker( locations[i] );
+    } 
+  }
+  initGoogleMap();
+
+
+  });
+}
+
 function google_map () {
   $.ajax({
     url: "/api/ip"
@@ -19,10 +121,11 @@ function google_map () {
       lat:json.lat,
       lng:json.lon
     }
-    new window.google.maps.Map(document.getElementById('map'), {
-      zoom: 10,
-      center: new window.google.maps.LatLng(CD)
-    });
+    // new window.google.maps.Map(document.getElementById('map'), {
+    //   zoom: 10,
+    //   center: new window.google.maps.LatLng(CD)
+    // });
+    start(CD.lat, CD.lng);
   });
 }
 google_map();
@@ -41,111 +144,109 @@ class LocationSearchInput extends React.Component {
     geocodeByAddress(address)
       .then(results => getLatLng(results[0]))
       .then(latLng => {
+        start(latLng.lat, latLng.lng);
+      //   $.ajax({
+      //     // url: "raw.json"
+      //     // url: "api/hello"
+      //     url: "https://yuriy.ca/recharge/?api_key=qweqwe123&lat="+ latLng.lat +"&lng="+ latLng.lng
+      //   }).done(function(resp) {
+      //     //var data = $.parseJSON(resp)
+      //     // console.log(resp);
 
-        $.ajax({
-          url: "raw.json"
-        }).done(function(data) {
-          // var data = $.parseJSON(resp)
-          console.log(data);
-
-        var locations = [];
-        for(var i=0; i < data.length; i++){
-          var smth = [data[i].LocName, data[i].Lat, data[i].Long, data[i].LocID,
-                      data[i].PortsUnknown, data[i].PortsAvail, data[i].PortsInUse ,data[i].PortsWorking,
-                      data[i].PortsOffline, data[i].Level3Ports, data[i].Level2Ports ,data[i].Level1Ports,
-                      data[i].Network, data[i].AccessCode, data[i].LocationTag];
-            locations.push(smth);
-        };
-        //console.log(locations);
-        function initGoogleMap(){
-            var bounds  = new window.google.maps.LatLngBounds();
-            var image = {url: 'charger.png'};
-            var infowindow = new window.google.maps.InfoWindow();
-            var map = new window.google.maps.Map(document.getElementById('map'), {
-              zoom: 8,
-              center: new window.google.maps.LatLng(latLng),
-              mapTypeControl: true,
-              mapTypeControlOptions: {
-                  style: window.google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-                  position: window.google.maps.ControlPosition.TOP_CENTER
-              },
-              zoomControl: true,
-              zoomControlOptions: {
-                  position: window.google.maps.ControlPosition.LEFT_CENTER
-              },
-              scaleControl: true,
-              streetViewControl: true,
-              streetViewControlOptions: {
-                  position: window.google.maps.ControlPosition.LEFT_TOP
-              },
-              fullscreenControl: true,
-              fullscreenControlOptions: {
-                position: window.google.maps.ControlPosition.BOTTOM_CENTER
-              }
-                  });
-          function placeMarker(loc) {
-            var latLng = new window.google.maps.LatLng( loc[1], loc[2]);
-            var marker = new window.google.maps.Marker({
-              position : latLng,
-              map      : map,
-              icon     : image,
-              animation:  window.google.maps.Animation.DROP
-            });
-            var locat = new window.google.maps.LatLng(marker.position.lat(), marker.position.lng());
-            bounds.extend(locat);
-            map.fitBounds(bounds);      
-            map.panToBounds(bounds); 
-            window.google.maps.event.addListener(marker, 'click', function(){
-                map.setZoom(10);
-                map.setCenter(this.getPosition());
-                infowindow.close(); // Close previously opened infowindow
-                infowindow.setContent( "<div id='infowindow'>"+ loc[0] +"</div>");
-                var network = "";
-                if(loc[12] === 19){
-                  network = "Volta";
-                } else if(loc[12] === 17){
-                  network = "No network";
-                }else if(loc[12] === 0){
-                  network = "Unknown network";
-                }
-                  else {
-                  network = "Other";
-                }
+      //   var locations = [];
+      //   // for(var i=0; i < data.length; i++){
+      //   //   var smth = [data[i].LocName, data[i].Lat, data[i].Long, data[i].LocID,
+      //   //               data[i].PortsUnknown, data[i].PortsAvail, data[i].PortsInUse ,data[i].PortsWorking,
+      //   //               data[i].PortsOffline, data[i].Level3Ports, data[i].Level2Ports ,data[i].Level1Ports,
+      //   //               data[i].Network, data[i].AccessCode, data[i].LocationTag];
+      //   //     locations.push(smth);
+      //   // };
+      //     for(var i=0; i < resp.length; i++){
+      //       var smth = [resp[i].id, resp[i].title, resp[i].address, resp[i].town, resp[i].country, resp[i].lat, resp[i].lng, 
+      //                   resp[i].postalcode, resp[i].phone, resp[i].numberofpoints, resp[i].comment, resp[i].powerkw];
+      //       locations.push(smth);
+      //   };
+      //   console.log(locations);
+      //   function initGoogleMap(){
+      //       var bounds  = new window.google.maps.LatLngBounds();
+      //       var image = {url: 'charger.png'};
+      //       var infowindow = new window.google.maps.InfoWindow();
+      //       var map = new window.google.maps.Map(document.getElementById('map'), {
+      //         zoom: 8,
+      //         center: new window.google.maps.LatLng(latLng),
+      //         mapTypeControl: true,
+      //         mapTypeControlOptions: {
+      //             style: window.google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      //             position: window.google.maps.ControlPosition.TOP_CENTER
+      //         },
+      //         zoomControl: true,
+      //         zoomControlOptions: {
+      //             position: window.google.maps.ControlPosition.LEFT_CENTER
+      //         },
+      //         scaleControl: true,
+      //         streetViewControl: true,
+      //         streetViewControlOptions: {
+      //             position: window.google.maps.ControlPosition.LEFT_TOP
+      //         },
+      //         fullscreenControl: true,
+      //         fullscreenControlOptions: {
+      //           position: window.google.maps.ControlPosition.BOTTOM_CENTER
+      //         }
+      //             });
+      //     function placeMarker(loc) {
+      //       var latLng = new window.google.maps.LatLng( loc[5], loc[6]);
+      //       var marker = new window.google.maps.Marker({
+      //         position : latLng,
+      //         map      : map,
+      //         icon     : image,
+      //         animation:  window.google.maps.Animation.DROP
+      //       });
+      //       var locat = new window.google.maps.LatLng(marker.position.lat(), marker.position.lng());
+      //       bounds.extend(locat);
+      //       map.fitBounds(bounds);      
+      //       map.panToBounds(bounds); 
+      //       window.google.maps.event.addListener(marker, 'click', function(){
+      //           map.setZoom(14);
+      //           map.setCenter(this.getPosition());
+      //           infowindow.close(); // Close previously opened infowindow
+      //           infowindow.setContent( "<div id='infowindow'>"+ loc[1] +"</div>");
                 
-                // var name = loc[0];
-                var info =
-                "<h3>" + loc[0].toUpperCase() + "</h3>" +
-                "<div class='_info'>" + 
-                  "<div class='_row'>" +
-                    "<div class='_col'>" +
-                      "<p><b>Network:</b> " + network + "</p>" +
-                      "<p><b>Location id:</b>  " + loc[3] + "</p>" +
-                      "<p><b>Ports available:</b>  " + loc[5] + "</p>" +
-                    "</div>" +
-                    "<div class='_col'>" +
-                      "<p><b>Ports in use:</b>  " + loc[6] + "</p>" +
-                      "<p><b>Ports working:</b>  " + loc[7] + "</p>" +
-                      "<p><b>Property:</b>  " + loc[14] + "</p>" +
-                    "</div>" +
-                  "</div>" +
-                "</div>";
+      //           var info =
+      //           "<h3>" + loc[1].toUpperCase() + "</h3>" +
+      //           "<div class='_info'>" + 
+      //             "<div class='_row'>" +
+      //               "<div class='_col'>" +
+      //                 "<p><b>Address:</b> " + loc[2] + "</p>" +
+      //                 "<p><b>City:</b>  " + loc[3] + "</p>" +
+      //                 "<p><b>Postal Code:</b>  " + loc[7] + "</p>" +
+      //               "</div>" +
+      //               "<div class='_col'>" +
+      //                 "<p><b>Phone:</b>  " + loc[8] + "</p>" +
+      //                 "<p><b>Comments:</b>  " + loc[10] + "</p>" +
+      //                 "<p><b>Ports available:</b>  " + loc[9] + "</p>" +
+      //               "</div>" +
+                    
+      //             "</div>" +
+      //           "</div>";
                
                 
                 
-                $('#information').html(info);
-                infowindow.open(map, marker);
-            });
-          }
-          for(var i=0; i<locations.length; i++) {
-            placeMarker( locations[i] );
-          } 
-        }
-        initGoogleMap();
+      //           $('#information').html(info);
+      //           infowindow.open(map, marker);
+      //       });
+      //     }
+      //     for(var i=0; i<locations.length; i++) {
+      //       placeMarker( locations[i] );
+      //     } 
+      //   }
+      //   initGoogleMap();
 
 
-        });
+      //   });
 
-      }) 
+      // 
+    
+    }) 
       .catch(error => console.error('Error', error))
   }
  
--- server.js
+++ server.js
@@ -1,27 +1,47 @@
 const express = require('express');
 var request = require('request');
+var url = require('url');
 const app = express();
 const port = process.env.PORT || 5000;
 
-var options = {
-    url: 'https://chargehub.azure-api.net/locationsdemo/',
-    headers: {
-      'Ocp-Apim-Subscription-Key': 'f53eee3a65f342e08e8ba7a453d95cce'
-    }
-  };
+
+// var options = {
+//     url: 'https://chargehub.azure-api.net/locationsdemo/',
+//     headers: {
+//       'Ocp-Apim-Subscription-Key': 'f53eee3a65f342e08e8ba7a453d95cce'
+//     }
+//   };
 var ip_options = {
     url: 'http://ip-api.com/json/'
 };
 
-app.get('/api/hello', function(req, res) {
-    request(options, function(error, response, body) {
-        res.json(body)
-    });
-});
+
+
+// app.get('/api/hello', function(req, res) {
+//     request(options, function(error, response, body) {
+//         res.json(body)
+//     });
+// });
+
+
+
+
 app.get('/api/ip', function(req, res) {
     request(ip_options, function(error, response, body) {
         res.json(body)
     });
 });
+
+// app.get('/api/geo/', function(req, res) {
+//     var geo = {
+//         url: 'https://yuriy.ca/recharge/?api_key='+api_key+'&lat='+lat+'&lng='+lng
+//     };
+//     var api_key = req.query.api_key;
+//     var lat = req.query.lat;
+//     var lng = req.query.lng;
+//     request(geo, function(error, response, body) {
+//         res.json(body)
+//     });
+// });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
